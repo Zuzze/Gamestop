2bfa34be1e924869b312f6591e8f07d34f2174ce
Rohan Krishnakumar <rohan@Rohans-MacBook-Pro.local>
2017-02-05 15:11:38 +0200

    Again

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import sys
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -87,6 +88,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
