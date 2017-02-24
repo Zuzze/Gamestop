046b2ba977db890d389468c784bcf1837be14d40
Rohan Krishnakumar <rohan@Rohans-MacBook-Pro.local>
2017-02-24 17:57:01 +0200

    Heroku Psotgres checkin

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = 'gamestore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gamestore',
+        'USER': 'gamestoreuser',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 #DATABASES['default'] =  dj_database_url.config()
 
