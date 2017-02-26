4667bc394e99eb072e95d83e6421e4f11297da6d
Rohan Krishnakumar <rohan@Rohans-MacBook-Pro.local>
2017-02-27 00:46:40 +0200

    Heroku

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -88,15 +88,15 @@ WSGI_APPLICATION = 'gamestore.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 """
+
 # Uncomment he following for Heroku and comment above
 DATABASES = {
     'default': {
@@ -109,7 +109,6 @@ DATABASES = {
 }
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
-"""
 
 #DATABASES['default'] =  dj_database_url.config()
 
